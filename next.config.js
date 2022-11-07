c49164ea6ca85aecd4cf2b3d8130096e28211512
Viperware Labs <111930874+viperware-labs@users.noreply.github.com>
2022-11-07 10:08:53 -0800

    API change

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     return [
       {
         // matching all API routes
-        source: "https://api.opensea.io/user/:path*",
+        source: "/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
